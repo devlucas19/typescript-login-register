fab4617ad0098b1a2353090e1b12e22491014ade
Lucas de Lima <101190090+devlucas19@users.noreply.github.com>
2026-04-22 19:51:04 -0300

    chore: initial server setup

---
+++ backend/src/index.ts
@@ -0,0 +1,50 @@
+import express from "express"
+import cors from "cors"
+
+import router from "./routes"
+import { pool } from "./database"
+
+const app = express()
+
+
+// Função de criação da tabela a qual deve ser executada no inicio do projeto.
+async function createTable(){
+    const tableQuery = 
+        `CREATE TABLE IF NOT EXISTS users(
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            fullName VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL,  
+            password VARCHAR(255) NOT NULL,  
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP  
+        )`
+
+    try {
+        const tableCreator = await pool.query(tableQuery)
+        console.log("Tabela criada com sucesso")
+    } catch (error) {
+        console.error(error)
+    }
+}
+// createTable()
+
+// Teste de conexão do banco de dados.
+async function connectionTest(){
+    try {
+        const connection = await pool.getConnection()
+        console.log("Conectado ao MySQL")
+        connection.release()
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+app.use(express.json())
+app.use(cors())
+app.use(router)
+
+
+app.listen(3000, ()=> {
+    connectionTest()
+    console.log("Server's running")
+})
+
